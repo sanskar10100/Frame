81393b289519f0d4af1bab4249e717d686405126
Sanskar Agrawal <sanskar10100@gmail.com>
2021-08-26 18:18:31 +0530

    Add offline data persistence

--- app/src/main/java/dev/sanskar/frame/ui/MainActivity.kt
+++ app/src/main/java/dev/sanskar/frame/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package dev.sanskar.frame.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import dev.sanskar.frame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Firebase.database.setPersistenceEnabled(true)
     }
 }
